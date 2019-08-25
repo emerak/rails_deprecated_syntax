32fd2602186b508a80fc884e11dd30117d38d41e
Alejandra Cernas <stephaniacernas@gmail.com>
2019-08-24 22:31:52 -0500

    Remove test

--- test/rails_deprecated_syntax.rb
+++ test/rails_deprecated_syntax.rb
@@ -3,7 +3,5 @@ require 'rails_deprecated_syntax'
 
 class RailsDeprecatedSyntax < Minitest::Test
   def test_english_hello
-    assert_equal "hello world",
-      RailsDeprecatedSyntax.check_deprecations
   end
 end
